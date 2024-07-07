fa0438dcf758b6242272bbb75fcd6ea78c0f3831
Mayank <innovativecreations195@gmail.com>
2024-07-07 23:35:43 +0530

    movement is done and flipping the cats

--- v2/script.js
+++ v2/script.js
@@ -2,49 +2,46 @@ document.addEventListener('keydown', handleKeyDown);
 document.addEventListener('keyup', handleKeyUp);
 
 const keysPressed = {};
-const step = 0.5;
-const moveInterval = 50;
+const step = 2;
+let animationFrameId;
 
 function handleKeyDown(event) {
     keysPressed[event.key] = true;
-    moveCats();
+    if (!animationFrameId) {
+        moveCats();
+    }
 }
 
 function handleKeyUp(event) {
     keysPressed[event.key] = false;
+    if (!Object.values(keysPressed).includes(true)) {
+        cancelAnimationFrame(animationFrameId);
+        animationFrameId = null;
+    }
 }
 
 function moveCats() {
     const cat1 = document.getElementById('cat1');
     const cat2 = document.getElementById('cat2');
 
-    let cat1Moved = false;
-    let cat2Moved = false;
-
     if (keysPressed['a']) {
         moveCat(cat1, -step);
         cat1.classList.add('flip-horizontal');
-        cat1Moved = true;
     }
     if (keysPressed['d']) {
         moveCat(cat1, step);
         cat1.classList.remove('flip-horizontal');
-        cat1Moved = true;
     }
     if (keysPressed['ArrowLeft']) {
         moveCat(cat2, -step);
         cat2.classList.remove('flip-horizontal');
-        cat2Moved = true;
     }
     if (keysPressed['ArrowRight']) {
         moveCat(cat2, step);
         cat2.classList.add('flip-horizontal');
-        cat2Moved = true;
     }
 
-    if (cat1Moved || cat2Moved) {
-        setTimeout(moveCats, moveInterval);
-    }
+    animationFrameId = requestAnimationFrame(moveCats);
 }
 
 function moveCat(cat, step) {
